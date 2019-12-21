4a3ded6abb3ad28751f5ae8b6a34e39352715ab6
Rakhman Gaibatov <gaibatov.ra@gmail.com>
2019-12-21 14:16:41 +0300

    Little change in GitHubProfile class

--- src/main/java/mini_webcrawler/profile/github/GitHubProfile.java
+++ src/main/java/mini_webcrawler/profile/github/GitHubProfile.java
@@ -1,104 +1,48 @@
-package mini_webcrawler.profile.github;
+package profile;
 
-import mini_webcrawler.profile.Profile;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GitHubProfile implements Profile {
     private String login;
     private String name;
-    private String company;
     private String location;
-    private String mostPopularLanguage;
-    private String mostPopularRepository;
-    private int starsInMostPopRepo;
-    private String ProgrammingLangInMostPopRepo;
-
-    GitHubProfile() {
-    }
-
-    void setLogin(String login) {
-        this.login = login;
-    }
-
-    void setName(String name) {
-        this.name = name;
-    }
-
-    void setCompany(String company) {
-        this.company = company;
-    }
-
-    void setLocation(String location) {
-        this.location = location;
-    }
-
-    void setMostPopularLanguage(String mostPopularLanguage) {
-        this.mostPopularLanguage = mostPopularLanguage;
-    }
-
-    void setMostPopularRepository(String mostPopularRepository) {
-        this.mostPopularRepository = mostPopularRepository;
-    }
-
-    void setStarsInMostPopRepo(int starsInMostPopRepo) {
-        this.starsInMostPopRepo = starsInMostPopRepo;
-    }
-
-    void setProgrammingLangInMostPopRepo(String programmingLangInMostPopRepo) {
-        ProgrammingLangInMostPopRepo = programmingLangInMostPopRepo;
-    }
+    private String company;
 
     public String getLogin() {
         return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getCompany() {
-        return company;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getLocation() {
         return location;
     }
 
-    public String getMostPopularLanguage() {
-        return mostPopularLanguage;
+    public void setLocation(String location) {
+        this.location = location;
     }
 
-    public String getMostPopularRepository() {
-        return mostPopularRepository;
+    public String getCompany() {
+        return company;
     }
 
-    public int getStarsInMostPopRepo() {
-        return starsInMostPopRepo;
-    }
-
-    public String getProgrammingLangInMostPopRepo() {
-        return ProgrammingLangInMostPopRepo;
-    }
-
-    public Map<String, String> getProfileProperties() {
-        Map<String, String> properties = new LinkedHashMap<>();
-        properties.put("login", getLogin());
-        properties.put("name", getName());
-        properties.put("company", getCompany());
-        properties.put("location", getLocation());
-        properties.put("mostPopularLanguage", getMostPopularLanguage());
-        properties.put("mostPopularRepository", getMostPopularRepository());
-        properties.put("starsInMostPopRepository", String.valueOf(getStarsInMostPopRepo()));
-        properties.put("ProgrammingLangInMostPopRepository", getProgrammingLangInMostPopRepo());
-        return properties;
+    public void setCompany(String company) {
+        this.company = company;
     }
 
     @Override
     public String toString() {
-        return String.format("login=%s, name=%s, company=%s, location=%s, mostPopularLanguage=%s, mostPopularRepository=%s, " +
-                "starsInMostPopRepository=%s, ProgrammingLangInMostPopRepository=%s", getLogin(), getName(), getCompany(), getLocation(), getMostPopularLanguage(),
-                getMostPopularRepository(), getStarsInMostPopRepo(), getProgrammingLangInMostPopRepo());
+        return String.format("login: %s, name: %s, location:  %s, company: %s", login, name, location, company);
     }
 }
